9b542b9cc7119f4253db5ee6e061cca7f44cd2ca
mymactive <60250695+miyazaki-masashi@users.noreply.github.com>
2022-03-14 21:54:04 +0900

    feat:example of axiosUtils and useAppSWR

---
+++ actions/petActions.ts
@@ -0,0 +1,18 @@
+import * as $axios from "../utils/axiosUtils";
+import { useAppSWR } from "../hooks/useAppSWR";
+
+const fetchAvailablePets = (name: string, photoUrls: string[]) =>
+  $axios.request({
+    url: "/pet",
+    method: "post",
+    data: {
+      name,
+      photoUrls,
+    },
+  });
+
+const { data, error } = useAppSWR({
+  url: "/pet/findByStatus",
+  method: "get",
+  params: { query: { status: ["pending"] } },
+});
